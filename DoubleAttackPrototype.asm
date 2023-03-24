6dc893d4a38cf2c4a14048b80d5ae0786165106c
inuksuktv <75352411+inuksuktv@users.noreply.github.com>
2023-03-24 11:44:56 -0600

    Bugfix
    Changed the comparison/branch on the double attack memory since it's initialized to FF at the start of battle.
    NB that using Crono's character stat block is hardcoded here. A proper solution would load the proper stat block offset (is it in Y?)

--- DoubleAttackPrototype.asm
+++ DoubleAttackPrototype.asm
@@ -1,21 +1,23 @@
 hirom
 
 org $c1c021     ; Location at the end of Prepare Attack routine.
-jsr $fadd       ; Jump to free space from Fist Upgrade Stopper hack.
+jsr $facd       ; Jump to free space from Fist Upgrade Stopper hack.
 
-org $c1fadd     ; Free space liberated by the Fist Upgrade Stopper hack.
+org $c1facd     ; Free space liberated by the Fist Upgrade Stopper hack.
 DoubleAttack:
 lda $b18b       ; Load attacker's battle ID.
 tax
 lda $aeff,X     ; Load attacker's index.
 cmp #$00        ; Compare to zero.
 bne .Cleanup    ; Branch if not Crono.
-lda $5e7c       ; Load temporary memory.
-cmp #$00        ; Compare to zero.
-bne .Cleanup    ; Branch if set.
-inc $5e7c       ; Increment double attack memory.
+lda $5e7c       ; Load double attack memory.
+cmp #$01        ; Compare to one.
+beq .Cleanup    ; Branch if one.
+lda #$01        ; Else load one.
+sta $5e7c       ; Store double attack memory.
 jsr $bfaa       ; Jump to prepare another attack.
 .Cleanup
-stz $5e7c       ; Clear double attack memory.
+lda #$ff        ; Load FF.
+sta $5e7c       ; Store to double attack memory.
 lda $b1fc       ; Command replaced by the JSR to get here.
 rts
