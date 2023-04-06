0cc84c51ab1d3654639c35e479eea2452434cc29
inuksuktv <75352411+inuksuktv@users.noreply.github.com>
2023-04-05 21:27:59 -0600

    Update CharmDrain.asm
    Hardcoded damage formula that deals 50% of attacker's max HP. Includes a check for Charm Top that makes it deal 100% max HP.

--- CharmDrain.asm
+++ CharmDrain.asm
@@ -10,35 +10,26 @@ org $01d3bf
 rts         ; Failsafe return for anything calling the old Stealing routine.
 DrainMode:
 jsr $e9a3   ; Get attacker's stat block offset.
-.LoopStart
 jsr $e9b8   ; Get defender's stat block offset.
-jsr $d14f   ; Get byte 2 and 3 of effect header.
-lda $16     ; Load HP/MP drain mode.
-sta $1e
-sta $20
-and #$80    ; Test HP mode.
-beq .MPDrain
 lda #$03    ; Load three.
 sta $1a     ; Store HP stat offset.
-; Calculate damage and store to $00.
-jsr $e7cb   ; Jump into the Drain execution routine after the damage calculation.
-lda $ad9b   ; Load effect header index.
-inc a       ; Increment.
-sta $ad9b   ; Store it.
-.MPDrain
-lda $1e     ; Load HP/MP drain mode.
-and #$40    ; Test MP mode.
-beq $08     ; Branch if not set.
-lda #$07    ; Load seven.
-sta $1a     ; Store MP stat offset.
-lda #$14    ; Load twenty.
-sta $16     ; Store to drain mode?
-; Calculate damage and store to $00.
+rep #$20    ; Set A 16-bit.
+clc
+adc $b1f4   ; Add to attacker's stat block offset.
+tax
+lda $5e2f,X ; Load attacker's max HP.
+sta $00     ; Scratch memory.
+sep #$20    ; Set A 8-bit.
+lda $5e54,X ; Load attacker's accessory.
+cmp #$a5    ; Compare to Charm Top.
+beq .Execute
+rep #$20    ; Set A 16-bit.
+lda $00     ; Load attacker's maxHP.
+lsr         ; /2
+sta $00     ; Store damage
+.Execute
 jsr $e7cb   ; Jump into the Drain execution routine after the damage calculation.
 jsr $d508   ; Load attack data.
-dec $ad8d   ; Decrement affected unit counter.
-lda $ad8d   ; Load it.
-bne .LoopStart
-lda #$00
+tdc
 sta $b200
 rts
