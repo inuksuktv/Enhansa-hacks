e164c7d8bbdc001cf93c9513def634d33cb4a8f6
inuksuktv <75352411+inuksuktv@users.noreply.github.com>
2023-04-26 12:54:50 -0600

    StartOfWork
    Wrote out the first few effect hooks for several characters. Work proceeds in tandem with Toma's event edits to set the relevant bitflags in memory.

--- DoubleAttackPrototype.asm
+++ DoubleAttackPrototype.asm
@@ -1,23 +1,30 @@
 hirom
 
+; This patch shares a co-dependency with my FistUpgradeStopper.asm hack. No particular reason, I just wrote them around the same time.
+
 org $c1c021     ; Location at the end of Prepare Attack routine.
 jsr $facd       ; Jump to free space from Fist Upgrade Stopper hack.
+nop #5
 
 org $c1facd     ; Free space liberated by the Fist Upgrade Stopper hack.
 DoubleAttack:
-lda $b18b       ; Load attacker's battle ID.
-tax
-lda $aeff,X     ; Load attacker's index.
-cmp #$00        ; Compare to zero.
-bne .Cleanup    ; Branch if not Crono.
-lda $5e7c       ; Load double attack memory.
-cmp #$01        ; Compare to one.
-beq .Cleanup    ; Branch if one.
-lda #$01        ; Else load one.
-sta $5e7c       ; Store double attack memory.
-jsr $bfaa       ; Jump to prepare another attack.
+phx
+ldx $b1f4       ; Load attacker's battle stat block local offset.
+lda $5e49,X     ; Load attacker's upgrade memory byte.
+bit #$08        ; Test "2x attack" upgrade bit.
+beq .Cleanup    ; Return if not set.
+lda $5e7c,X     ; Load attacker's temp memory byte (default FF, unused).
+bit #$01
+beq .Cleanup    ; Return if not set.
+and #$fe        ; Else clear bit 01.
+sta $5e7c,X     ; Store memory.
+jsr $bfaa       ; Prepare a second attack.
 .Cleanup
-lda #$ff        ; Load FF.
-sta $5e7c       ; Store to double attack memory.
-lda $b1fc       ; Command replaced by the JSR to get here.
+lda $5e7c,X     ; Load 2x attack memory.
+ora #$01        ; Set bit 01.
+sta $5e7c,x     ; Store 2x attack memory.
+plx
+lda $b1fc       ; This section of cleanup replaced by the JSR to get here.
+and #$fd        ; Clear bit 02
+sta $b1fc
 rts
--- EffectHeaderHook.asm
+++ EffectHeaderHook.asm
@@ -7,33 +7,83 @@ nop #6          ; The setup is ten bytes total. Setup moved to the cleanup secti
 org $5f0110
 EffectHeaderHook:
 php             ; Push flags.
-.Marle
 lda $b18b       ; Load attacker's battle ID.
+sta $00         ; Store it.
+.Crono
 tax
 lda $aeff,X     ; Load PC ID.
-sta $00         ; Store it.
-cmp #$01        ; Compare to one.
-bne .Cleanup    ; Branch if not Marle.
+sta $02
+cmp #$00        ; Compare to zero.
+bne .Marle      ; Branch if not Crono.
+.Cyclone
 lda $b2c8       ; Load effect header.
-cmp #$09        ; Compare to 09.
-bne .Cleanup    ; Branch if Tech is not Aura.
-lda $00         ; Load PC ID.
-rep #$20        ; Set A 16-bit.
-xba
-lsr a
-tax             ; Convert to stat block offset.
-tdc
-sep #$20        ; Set A 8-bit
-lda $5e49,X     ; Load Marle's upgrade memory byte.
+cmp #$01        ; Compare to one.
+bne .Marle      ; Branch if Tech is not Cyclone.
+lda $261c       ; Load Crono's upgrade memory byte.
+bit #$01        ; Test bit $01.
+beq .Marle      ; Branch if bit $01 not set.
+lda $aeef       ; Load Tech Power (isCrono, isCyclone, isUpgraded all passed).
+lsr #2          ; /4
+clc
+adc $aeef       ; Add TP + TP/4.
+sta $aeef       ; Store 1.25*TP
+bra .Marle
+.Marle
+lda $00         ; Load attacker's battle ID.
+tax
+lda $aeff,X     ; Load PC ID.
+cmp #$01        ; Compare to one.
+bne .Lucca      ; Branch if not Marle.
+.Aura
+lda $b2c8       ; Load effect header.
+cmp #$09        ; Compare to nine.
+bne .Lucca      ; Branch if Tech is not Aura.
+lda $266c       ; Load Marle's upgrade memory byte.
 bit #$01
-beq .Cleanup    ; Branch if bit 01 not set.
+beq .Lucca      ; Branch if bit 01 not set.
 lda #$02        ; (isMarle, isAura, isUpgraded all passed)
 sta $aee9       ; Set status mode to 02.
 lda #$40
 sta $aeea       ; Set status bitflag to $40 (Regen).
-lda #$20
-sta $aeed       ; Set always hits flag.
-bra .Cleanup
+bra .Lucca
+.Lucca
+lda $00         ; Load attacker's battle ID.
+tax
+lda $aeff,X     ; Load PC ID.
+cmp #$02        ; Compare to two.
+bne .Robo       ; Branch if not Lucca.
+.HypnoWave
+lda $b2c8       ; Load effect header.
+cmp #$12        ; Compare to $12
+bne .Robo       ; Branch if Tech is not Hypno Wave
+lda $26bc       ; Load Lucca's upgrade memory byte.
+bit #$01
+beq .Robo       ; Branch if bit 01 not set.
+lda $aeed       ; (isLucca, isHypnoWave, isUpgraded all passed.)
+ora #$20
+sta $aeed       ; Set always hit flag.
+bra .Robo
+.Robo
+lda $00         ; Load attacker's battle ID.
+tax
+lda $aeff,X     ; Load PC ID.
+cmp #$03        ; Compare to three.
+bne .Frog       ; Branch if not Robo.
+.LaserSpin
+lda $b2c8       ; Load effect header.
+cmp $1b
+bne .Frog       ; Branch if Tech is not Laser Spin.
+lda $270c       ; Load Robo's upgrade memory byte.
+bit #$01
+beq .Frog
+lda $aeef       ; Load Tech Power (isRobo, isLaserSpin, isUpgraded all passed).
+lsr #2          ; /4
+clc
+adc $aeef       ; Add TP + TP/4.
+sta $aeef       ; Store 1.25*TP
+.Frog
+.Ayla
+.Magus
 .Cleanup
 plp             ; Pull flags.
 lda $aee6       ; Load Tech mode.
