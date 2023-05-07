59b236850a28ae07216fbdecd9d8ddc89c7cc715
inuksuktv <75352411+inuksuktv@users.noreply.github.com>
2023-05-07 16:13:22 -0600

    TechRepeater
    Implemented a Tech Repeater hook. Similar to Double Attack in principle.

--- DoubleAttackHook.asm
+++ DoubleAttackHook.asm
@@ -1,6 +1,6 @@
 hirom
 
-; This patch shares a co-dependency with my FistUpgradeStopper.asm hack. No particular reason, I just wrote them around the same time.
+; This patch depends with my FistUpgradeStopper.asm hack. No particular reason, I just wrote them around the same time.
 
 org $c1c021     ; Location at the end of Prepare Attack routine.
 jsr $facd       ; Jump to free space from Fist Upgrade Stopper hack.
@@ -13,7 +13,7 @@ ldx $b1f4       ; Load attacker's battle stat block local offset.
 lda $5e49,X     ; Load attacker's upgrade memory byte.
 bit #$08        ; Test "2x attack" upgrade bit.
 beq .Cleanup    ; Return if not set.
-lda $5e7c,X     ; Load attacker's temp memory byte (default FF, unused).
+lda $5e7c,X     ; Load attacker's temp memory byte (unused, default FF).
 bit #$01
 beq .Cleanup    ; Return if not set.
 and #$fe        ; Else clear bit 01.
@@ -24,7 +24,7 @@ lda $5e7c,X     ; Load 2x attack memory.
 ora #$01        ; Set bit 01.
 sta $5e7c,x     ; Store 2x attack memory.
 plx
-lda $b1fc       ; This section of cleanup replaced by the JSR to get here.
-and #$fd        ; Clear bit 02
+lda $b1fc       ; This section of cleanup was overwritten by the JSR to get here.
+and #$fd
 sta $b1fc
 rts
---
+++ TechRepeaterHook.asm
@@ -0,0 +1,32 @@
+exhirom
+
+org $c1bc4a     ; Location in the Tech command stream after animation and some Charm processing.
+jsl $5f0080
+bit #$80        ; Test bit $80 of temp memory byte.
+bne $03         ; Continue if set.
+jsr $bb49       ; Else call another Tech.
+nop
+
+org $5f0080     ; Free space in expanded rom
+TechRepeater:   ; A holds control header. X holds PC ID.
+lda $b1be,X     ; Load attacker's battle ID.
+xba
+lsr
+tax             ; Convert battle ID to stat block offset.
+lda $aecc       ; This and the next four lines written over by the hook at $c1bc4a.
+cmp #$03        ; Compare to three.
+bcc $05         ; Branch if target is PC.
+lda #$01
+sta $b2c0       ; Set counterattack flag for enemy.
+lda $5e7c,X     ; Load attacker's temp memory byte (unused, default FF).
+bit #$80        ; Test 2x Tech bit.
+beq .Cleanup    ; Branch if not set.
+and #$7f        ; Else clear bit 80.
+sta $5e7c,X     ; Store memory.
+bra .Return
+.Cleanup
+lda $5e7c,X     ; Load 2x Tech memory.
+ora #$80        ; Set bit 80.
+sta $5e7c,x     ; Store 2x Tech memory.
+.Return
+rtl
