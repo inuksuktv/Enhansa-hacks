b50b7add1dcc2dab6a09558449ab22c7549dcf3a
inuksuktv <75352411+inuksuktv@users.noreply.github.com>
2024-08-03 21:10:58 -0600

    MuscleBeltFix
    Fixed a bug where Muscle Belt wasn't applying its HP bonus in battle.

--- EarringMuscleBelt.asm
+++ EarringMuscleBelt.asm
@@ -1,11 +1,11 @@
 exhirom
 
-org $c2929d     ; Hook for HP calculation.
+org $c2929d     ; Hook for menu HP bonus.
 jsl $5f0470
 nop #9
 
 org $5f0470     ; Free space in expanded ROM.
-Main:
+First:
 cpx #$a1        ; Compare to Gold Earring.
 beq .OneHalf
 cpx #$a0        ; Compare to Silver Earring.
@@ -19,12 +19,40 @@ lsr
 lsr
 rtl
 
+org $fdb3aa     ; Hook for battle HP bonus.
+Second:
+lda $5e57,x     ; Load accessory ID.
+tay
+rep #$20
+lda $5e32,X     ; Load max HP.
+cpy #$00a1      ; Compare to Gold Earring.
+beq .AHalf
+cpy #$00a0      ; Compare to Silver Earring.
+beq .AQuarter
+cpy #$00b4      ; Compare to Muscle Ring.
+beq .AQuarter
+lda #$0000
+.AQuarter
+lsr
+.AHalf
+lsr
+clc
+adc $5e32,X
+cmp #$03e7
+bcc $03
+lda #$03e7      ; 999 ceiling
+sta $5e32,X     ; Store max HP.
+tdc
+sep #$20
+rtl
+nop #17
+
 org $c28d26     ; Hook for HP restore points
 jsl $5f0510
 nop #11
 
 org $5f0510     ; Free space in expanded ROM.
-SecondMain:
+Third:
 cpx #$00a1      ; Compare to Gold Earring.
 beq .Half
 cpx #$00a0      ; Compare to Silver Earring.
